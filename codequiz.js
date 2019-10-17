94bba7df548ddde465d1ff5a6ac352443fdccbc3
David Vega <dvega920@gmail.com>
2019-10-17 15:25:04 -0400

    added getQuestions function, revised event listener

--- codequiz.js
+++ codequiz.js
@@ -19,18 +19,60 @@ FUNCTIONALITY
 
 */
 
-
-var count = 5 //seconds;
 var timer = document.querySelector('.timer-color');
-var reset = 0;
+var p = document.querySelector('.card-text');
 var btn = document.querySelector('.btn');
+var count = 5 //seconds;
+var stopTimer = 0;
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+];
 
-//event listener to start timer
+p.textContent = "Quiz objective is to answer as many questions correctly, if incorrect answers is selected timer will decrement 5 seconds";
+
+//event listener to start timer and quiz
 btn.addEventListener('click', function () {
-    var myCounter = setInterval(function () {
-        count--;
-        timer.textContent = 'Timer: ' + count;
-    }, 1000)
-})
+    if (count >= stopTimer) {
+        var myCounter = setInterval(function () {
+            count--;
+            timer.textContent = 'Timer: ' + count;
+
+            //function to loop through questions array and console log them
+            function getQuestions() {
+                questions.forEach(function (item) {
+                    this.questions.title;
+                })
+            };
+            getQuestions(); //function call
+            // var ulEl = document.querySelector('.list');
+            var btnEl1 = document.querySelector('.choice1');
+            var btnEl2 = document.querySelector('.choice2');
+            var btnEl3 = document.querySelector('.choice3');
+            var btnEl4 = document.querySelector('.choice4');
+
+            btnEl1.textContent = questions[0].choices[0];
+            btnEl2.textContent = questions[0].choices[1];
+            btnEl3.textContent = questions[0].choices[2];
+            btnEl4.textContent = questions[0].choices[3];
+        }, 1000)
+        //need to clearInterval at somepoint 
+        // p.textContent = questions[0].title;
+
+
+
+
+    }
+});
 // click event works but if clicked again, timer goes all "wonky"
+
+
 
