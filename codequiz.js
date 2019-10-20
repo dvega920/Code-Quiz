eeddc416a1aecdb06dc5becda6000a9839cd0345
David Vega <dvega920@gmail.com>
2019-10-20 12:17:55 -0400

    trying to get choices to print to page. Only prints out last item in choices array or all of them on a single line

--- codequiz.js
+++ codequiz.js
@@ -3,25 +3,8 @@ $(document).ready(function(){
 var timer = $('.timer-color');
 var p = $('.card-text');
 var btn = $('.btn');
-var count = 75; //seconds
-var stopTimer = 0;
 
-var questions = [
-    {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-];
-
-// console.log(questions[0].choices);
-
-//function to stop and clear timer
+//function to stop and clear timer. Takes in argument "x" which will pass "myCounter" which is the variable that stores the setInterval(). 
 function stopFunction(x) {
     clearInterval(x);
 }
@@ -33,51 +16,75 @@ function createEl(el) {
     $(".card-body").append(el);
 }
 
+//function to store current score
+function scoreTracker(){
+    var score = 0;
+    if (count >= 0){
+        score++;
+    }
+}
+
+//Game objective text on landing page
 p.text("Quiz objective is to answer as many questions correctly, if incorrect answers is selected timer will decrement 5 seconds");
 
 
-// console.log(questions[0].choices.length);
-
-
-//event listener to start timer and quiz
-
+//jQuery event listener to start timer and quiz
 btn.click(function () {
 
+    var questions = [
+        {
+            title: "Commonly used data types DO NOT include:",
+            choices: ["strings", "booleans", "alerts", "numbers"],
+            answer: "alerts"
+        },
+        {
+            title: "The condition in an if / else statement is enclosed within ____.",
+            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+            answer: "parentheses"
+        },
+    ];
+    
+    var count = 75; //store starting value of timer.
+    var stopTimer = 0; // value which timer must end
+
+    //Test to check for value of 0 for timer
     if (count >= stopTimer) {
         var myCounter = setInterval(function () {
             count--;
             timer.text('Timer: ' + count);
 
             if (count === 0) {
-                // clearInterval(myCounter);
+                // function call to stop timer at 0 and takes in argument of myCounter variable set to setInterval()
                 stopFunction(myCounter);
             }
+            
         }, 1000)
         
-        //removes unwanted elements
-        $("h5").remove();
-        $(".card-text").remove();
-        $(".submit").remove();
+        //hides unwanted elements from the questions screen
+        $("h5").hide();
+        $(".card-text").hide();
+        $(".submit").hide();
         
-        //Sets the first question and series of options
+        //changes card-header class to read "Question: 1", create an <h2> element addClass to <h2>, adds first question to newly created element.
         $(".card-header h1").text("Question 1:");
         createEl("<h2>");
         $("h2").addClass("questions-title");
         $(".questions-title").text(questions[0].title);
 
-        
-        //Loop to iterate through choices for questions array at index 0
-        for (var i = 0; i < questions[0].choices.length; i++) {
-            // console.log(questions[0].choices.length);
-            createEl("<a href=\"\#\" class=" + "choice-" + i + ">");
-            $('a').addClass("btn btn-primary choicesProp")
-            // console.log(questions[0].choices[i]);
-            $(".choice-" + i).text(questions[0].choices[i]);
-            createEl("<br>");
-            $(".choice-" + i).append("<br>");
+       var choices = questions[0].choices; 
+
+       $.each(choices,function(index, value){
+        createEl("<a>")
+        $("<a>").append(".card-body");
+        $("<a>").text(value);
+       })
+      
+
+          
+
+     
+
+            
         }
-
-
-    }
-})
+    })
 });
