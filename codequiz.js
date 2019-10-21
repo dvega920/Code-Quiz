42332f4d4fba47eb4c39ff593b5ee6ef3d9f85d5
David Vega <dvega920@gmail.com>
2019-10-20 22:38:46 -0400

    trying to get choices to print

--- codequiz.js
+++ codequiz.js
@@ -1,8 +1,21 @@
-$(document).ready(function(){
-    
-var timer = $('.timer-color');
-var p = $('.card-text');
-var btn = $('.btn');
+var $timer = $('.timer-color');
+var $p = $('.card-text');
+var $btn = $('.btn');
+var currentIndex = 0;
+var penalty = 5;
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+];
+
 
 //function to stop and clear timer. Takes in argument "x" which will pass "myCounter" which is the variable that stores the setInterval(). 
 function stopFunction(x) {
@@ -16,75 +29,45 @@ function createEl(el) {
     $(".card-body").append(el);
 }
 
-//function to store current score
-function scoreTracker(){
-    var score = 0;
-    if (count >= 0){
-        score++;
-    }
+function getQuestions() {
+    questions.forEach(function (item) {
+        $(".card-body").html("<a>" + item.title);
+    })
 }
 
 //Game objective text on landing page
-p.text("Quiz objective is to answer as many questions correctly, if incorrect answers is selected timer will decrement 5 seconds");
+$p.text("Quiz objective is to answer as many questions correctly before time runs out, if incorrect answer is selected timer will decrement 5 seconds");
 
 
-//jQuery event listener to start timer and quiz
-btn.click(function () {
-
-    var questions = [
-        {
-            title: "Commonly used data types DO NOT include:",
-            choices: ["strings", "booleans", "alerts", "numbers"],
-            answer: "alerts"
-        },
-        {
-            title: "The condition in an if / else statement is enclosed within ____.",
-            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-            answer: "parentheses"
-        },
-    ];
-    
+function startQuiz() {
     var count = 75; //store starting value of timer.
     var stopTimer = 0; // value which timer must end
+
+    //hides unwanted elements from the questions screen
+    $("h5").hide();
+    $(".card-text").hide();
+    $(".submit").hide();
 
     //Test to check for value of 0 for timer
     if (count >= stopTimer) {
         var myCounter = setInterval(function () {
             count--;
-            timer.text('Timer: ' + count);
+            $timer.text('Timer: ' + count);
 
             if (count === 0) {
                 // function call to stop timer at 0 and takes in argument of myCounter variable set to setInterval()
                 stopFunction(myCounter);
             }
-            
         }, 1000)
-        
-        //hides unwanted elements from the questions screen
-        $("h5").hide();
-        $(".card-text").hide();
-        $(".submit").hide();
-        
-        //changes card-header class to read "Question: 1", create an <h2> element addClass to <h2>, adds first question to newly created element.
-        $(".card-header h1").text("Question 1:");
-        createEl("<h2>");
-        $("h2").addClass("questions-title");
-        $(".questions-title").text(questions[0].title);
+    }
+    createEl("<h4>");
+    $("h4").addClass("question1");
+    $(".question1").append(questions[0].title);
 
-       var choices = questions[0].choices; 
+    createEl("<a href=\"\#\" class=\"choice1\">\"");
+    $(".choice1").append(questions[0].choices)
+}
 
-       $.each(choices,function(index, value){
-        createEl("<a>")
-        $("<a>").append(".card-body");
-        $("<a>").text(value);
-       })
-      
-
-          
-
-     
-
-            
-        }
-    })
-});
+$btn.click(function () {
+    startQuiz();
+})
